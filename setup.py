6cb790a6267061053c171d375a0311a5cb525ab1
vasudevansoni <vasudevansoni07@gmail.com>
2023-08-23 13:29:36 +0530

    'newsetup'

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path:str)->List[str]:
         requirements = [req.replace('\n',"") for req in requirements]
     
     if HYPEN_E_DOT in requirements:
-        requirements.remove()
+        requirements.remove(HYPEN_E_DOT)
     
     return requirements
 
